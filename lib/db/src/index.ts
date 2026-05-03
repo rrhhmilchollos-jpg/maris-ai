513f1e04e9d3a041ffcc28a6ff58bc04afaa0c64
rrhhmilchollos-jpg <rrhh.milchollos@gmail.com>
2026-05-03 17:10:23 +0200

    Refactor exports in index.ts for clarity

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -1,2 +1,9 @@
 export * from "./schema/index.js";
+export {
+  GeneratedApp as generatedApps,
+  AppRuntimeError as appRuntimeErrors,
+  AppImage as appImages,
+  AgentMemory as agentMemory,
+  ChatAttachment as chatAttachments,
+} from "./schema/index.js";
 export { connectDB, default as connectDBDefault } from "./db.js";
