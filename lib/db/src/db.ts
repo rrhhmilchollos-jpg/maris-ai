8e6b38dc18de7a16e774d02e7a92a0e8699b980c
rrhhmilchollos-jpg <rrhh.milchollos@gmail.com>
2026-05-03 16:15:37 +0200

    Implement MongoDB connection utility

---
+++ lib/db/src/db.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+if (!process.env.MONGODB_URI) {
+  throw new Error("MONGODB_URI must be set");
+}
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+declare global {
+  var _mongooseConnection: Promise<typeof mongoose> | undefined;
+}
+
+async function connectDB(): Promise<typeof mongoose> {
+  if (global._mongooseConnection) {
+    return global._mongooseConnection;
+  }
+  global._mongooseConnection = mongoose.connect(MONGODB_URI, {
+    serverSelectionTimeoutMS: 10_000,
+    socketTimeoutMS: 45_000,
+  });
+  return global._mongooseConnection;
+}
+
+export { connectDB };
+export default connectDB;
